e129dd5e910ff4b0f449858c2ff2065c2dfa4082
Elfico06 <100107313+Elfico06@users.noreply.github.com>
2022-03-04 00:16:51 -0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,10 @@
+const nombre = document.getElementById("name");
+const mail = document.getElementById("mail");
+const msg = document.getElementById("msg");
+const listinputs = document.querySelectorAll("li")
+
+Form addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if nombre.ariaValueMax.length < 1 
+});
